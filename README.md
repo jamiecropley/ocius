a1d799c506a1c16dae2afe79b21100af9dfbcb24
Jamie Cropley <jamie.cropley@protonmail.com>
2023-07-15 01:25:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ocius
+
+## Main site:
+
+https://ocius.xyz
+
+## Socials:
+
+- https://discord.gg/bMnDf965Pu
+- https://facebook.com/ociuse
+- https://instagram.com/ociuse
+- https://linkedin.com/company/ociuse
+- https://threads.net/@ociuse
+- https://tiktok.com/@ociuse
+- https://twitter.com/ociuse
+- https://youtube.com/@ociuse
+
+Email:
+
+- jamie@ocius.xyz
+
+
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+/* The below code will change at some point, the window will change to a separate file as a class,
+ the below code is more for testing at this point. */
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "ocius", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Print a message to the command prompt */
+    std::cout << "Welcome to the application! The window should now be open." << std::endl;
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT); // uncomment when I add OpenGL
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
