3e8b91857bd2abe50718f6e765acf44ad490781a
Jamie Cropley <jamie.cropley@protonmail.com>
2023-07-18 19:35:15 +0100

    Updated math engine

--- main.cpp
+++ main.cpp
@@ -29,15 +29,62 @@ int main(void)
     std::cout << "- Window has opened." << std::endl;
     std::cout << "- OpenGL version: " << version << std::endl;
 
-    // Define two vectors
+
+
+
+    // MATH ENGINE BASIC TESTING
+
+    // VECTOR CLASS BEGIN:
+    //VECTORS:
+    // These vector values are just for testing! Final version will be seperated out of main.cpp into reusable classes.
     glm::vec3 vec1(1.0f, 2.0f, 3.0f);
     glm::vec3 vec2(4.0f, 5.0f, 6.0f);
 
+    // ADDITION BEGIN:
     // Add the vectors together
     glm::vec3 result = vec1 + vec2;
-
     // Print out the result
     std::cout << "Vector addition result: " << result.x << ", " << result.y << ", " << result.z << std::endl;
+    // ADDITION END.
+
+    // SUBTRACTION BEGIN:
+    // Subtract the vectors
+    glm::vec3 result_subtraction = vec1 - vec2;
+    // Print out the result
+    std::cout << "Vector subtraction result: " << result_subtraction.x << ", " << result_subtraction.y << ", " << result_subtraction.z << std::endl;
+    // SUBTRACTION END:
+
+    //DOT PRODUCT BEGIN:
+    // Calculate the dot product of the vectors
+    float dot_product = glm::dot(vec1, vec2);
+    // Print out the result
+    std::cout << "Dot product result: " << dot_product << std::endl;
+    //DOT PRODUCT END.
+
+    // CROSS PRODUCT BEGIN:
+    // Calculate the cross product of the vectors
+    glm::vec3 cross_product = glm::cross(vec1, vec2);
+    // Print out the result
+    std::cout << "Cross product result: " << cross_product.x << ", " << cross_product.y << ", " << cross_product.z << std::endl;
+    // CROSS PRODUCT END.
+
+    // VECTOR CLASS END.
+
+    // MATRIX CLASS BEGIN:
+
+    // I need to look into the mathematics before starting this.
+    // Do I need to do just rotation, or translation and scaling as well?
+
+    // I need to do:
+    // - Transform
+    // - Invert
+    // - Transpose
+    // - Identity
+
+    // MATRIX CLASS END.
+
+
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
